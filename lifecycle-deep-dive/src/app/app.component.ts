26ab67fca25842b22b06290a2d8fdea906e9f1f5
Rahul123 <pr@unstoppablesoftwares.com>
2025-10-15 07:14:42 +0530

    test

---
+++ lifecycle-deep-dive/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { LifecycleComponent } from './lifecycle/lifecycle.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [LifecycleComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+   lifecycleComponentIsVisible = false;
+  lifecycleInputText = 'Some Random Number: ' + Math.random() * 100;
+
+  onToggleLifecycleComponentVisibility() {
+    this.lifecycleComponentIsVisible = !this.lifecycleComponentIsVisible;
+  }
+
+  onChangeLifecycleInputText() {
+    this.lifecycleInputText = 'Some Random Number: ' + Math.random() * 100;
+  }
+}
---
+++ lifecycle-deep-dive/src/app/lifecycle/lifecycle.component.ts
@@ -0,0 +1,58 @@
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, 
+  DoCheck, Input, OnChanges, OnDestroy, OnInit ,SimpleChanges} from '@angular/core';
+
+@Component({
+  selector: 'app-lifecycle',
+  imports: [],
+  templateUrl: './lifecycle.component.html',
+  styleUrl: './lifecycle.component.css'
+})
+export class LifecycleComponent implements
+    OnInit,
+    OnChanges,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked,
+    OnDestroy {
+@Input() text?: string;
+
+constructor() {
+    console.log('CONSTRUCTOR');
+  }
+
+  ngOnInit() {
+    console.log('ngOnInit');
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChanges');
+    console.log(changes);
+  }
+
+  ngDoCheck() {
+    console.log('ngDoCheck');
+  }
+
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit');
+  }
+
+  ngAfterContentChecked() {
+    console.log('ngAfterContentChecked');
+  }
+
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit');
+  }
+
+  ngAfterViewChecked() {
+    console.log('ngAfterViewChecked');
+  }
+
+  ngOnDestroy() {
+    console.log('ngOnDestroy');
+  }
+
+}
