4e5629f2b877f1337558445c398f35d4079536f4
Rahul123 <pr@unstoppablesoftwares.com>
2025-11-06 07:07:29 +0530

    done

--- HTTP/src/app/places/available-places/available-places.component.ts
+++ HTTP/src/app/places/available-places/available-places.component.ts
@@ -3,6 +3,7 @@ import { PlacesContainerComponent } from "../places-container/places-container.c
 import { PlacesComponent } from "../places.component";
 import { Place } from '../place.model';
 import { HttpClient } from '@angular/common/http';
+import { catchError, map, single, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-available-places',
@@ -12,18 +13,48 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AvailablePlacesComponent implements OnInit {
   places = signal<Place[] | undefined>(undefined);
+  isLoading=signal(false);
+  error=signal('');
   private httpClient=inject(HttpClient);
   private detroyRef=inject(DestroyRef);
 
   ngOnInit(): void {
-    const subscription= this.httpClient.get<{places:Place[]}>('http://localhost:300/places').subscribe({
-      next:(resData) => {
-          console.log(resData);
+    this.isLoading.set(true);
+    const subscription= this.httpClient.get<{places:Place[]}>('http://localhost:3000/places')
+    .pipe( map((response) => response.places ),
+    catchError((err) => {
+      console.log(err);
+      return throwError(
+        () =>{
+        new Error("Something Went Wrong pls contact administrator")
+      })
+    })
+  )
+    .subscribe({
+      next:(response) => {
+          this.places.set(response)
+      },
+      error:(err:Error) => {
+        debugger;
+          this.error.set(err.message)
+      },
+      complete:()=>{
+        this.isLoading.set(false);
       }
     })
+    
 
     this.detroyRef.onDestroy(() => {
         subscription.unsubscribe();
+    })
+  }
+
+  onselectPalce(selectPalce:Place){
+    this.httpClient.put('http://localhost:3000/user-places',{
+      placeId:selectPalce.id
+    })
+    .subscribe(() => {
+      next:(resData:any) => console.log(resData)
     })
   }
 }
